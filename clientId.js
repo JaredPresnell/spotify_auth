4f882c1eebc28a1ced7b893c517a4d61b50a6035
JaredPresnell <jared.presnell@gmail.com>
2019-04-05 16:23:30 -0500

    first commit

---
+++ clientId.js
@@ -0,0 +1,3 @@
+var client_id = 'd86ec0cc64b3479c8f81a17b9e7985e2'; // Your client id
+
+module.exports =  client_id;
---
+++ clientSecret.js
@@ -0,0 +1,3 @@
+var client_secret = 'e4de092c377545ac919d2ac66d9b4eca'; // Your secret
+
+module.exports = client_secret;
